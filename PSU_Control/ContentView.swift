2e4eb3d6eaf8e425c84b79f2d0c743587e40910e
Jim Kardach <jim@kardach.clom>
2025-02-23 12:37:19 -0800

    Initial Commit

---
+++ PSU_Control/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PSU_Control
+//
+//  Created by jim kardach on 2/23/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PSU_Control/PSU_ControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  PSU_ControlApp.swift
+//  PSU_Control
+//
+//  Created by jim kardach on 2/23/25.
+//
+
+import SwiftUI
+
+@main
+struct PSU_ControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
